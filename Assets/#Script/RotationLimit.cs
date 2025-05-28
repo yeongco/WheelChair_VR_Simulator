e77e2b4c73fb4ad053aed057bb68dbcba34dbdc9
mindrare <sohochan1262@gmail.com>
2025-05-28 17:59:49 +0900

    wheelchair_implementing_0528

--- Assets/#Script/RotationLimit.cs
+++ Assets/#Script/RotationLimit.cs
@@ -7,16 +7,16 @@ public class RotationLimit : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 rot = transform.eulerAngles;
-        // x축 회전 각도가 30도 이상(뒤로 넘어가는 각도)이면 30도로 고정
-        // Unity의 EulerAngles는 0~360도로 표현되므로, 330~360도(즉, -30도)도 체크
-        if (rot.x > 30f && rot.x < 180f)
+        // x축 회전 각도가 15도 이상(뒤로 넘어가는 각도)이면 15도로 고정
+        // Unity의 EulerAngles는 0~360도로 표현되므로, 345~360도(즉, -15도)도 체크
+        if (rot.x > 15f && rot.x < 180f)
         {
-            rot.x = 30f;
+            rot.x = 15f;
             transform.eulerAngles = rot;
         }
-        else if (rot.x > 180f && rot.x < 330f)
+        else if (rot.x > 180f && rot.x < 345f)
         {
-            rot.x = 330f;
+            rot.x = 345f;
             transform.eulerAngles = rot;
         }
     }
