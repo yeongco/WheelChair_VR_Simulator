af232a2c2bee3728c9291c093fdec9e736eb723e
mindrare <sohochan1262@gmail.com>
2025-06-01 00:41:05 +0900

    WheelChair_FIX
    불필요한 prefab 파일 제거 및 RotationLimit 파일 수정(회전 제한값 수정)
    휠체어 drag값 수정

--- Assets/#Script/RotationLimit.cs
+++ Assets/#Script/RotationLimit.cs
@@ -4,22 +4,19 @@ using UnityEngine;
 
 public class RotationLimit : MonoBehaviour
 {
-    [SerializeField, Range(0f, 90f)]
-    private float maxRotationAngle = 5f;  // 기본값 5도로 설정
-
     void FixedUpdate()
     {
         Vector3 rot = transform.eulerAngles;
-        // x축 회전 각도가 maxRotationAngle 이상(뒤로 넘어가는 각도)이면 maxRotationAngle로 고정
-        // Unity의 EulerAngles는 0~360도로 표현되므로, (360-maxRotationAngle)~360도(즉, -maxRotationAngle도)도 체크
-        if (rot.x > maxRotationAngle && rot.x < 180f)
+        // x축 회전 각도가 15도 이상(뒤로 넘어가는 각도)이면 15도로 고정
+        // Unity의 EulerAngles는 0~360도로 표현되므로, 345~360도(즉, -15도)도 체크
+        if (rot.x > 15f && rot.x < 180f)
         {
-            rot.x = maxRotationAngle;
+            rot.x = 15f;
             transform.eulerAngles = rot;
         }
-        else if (rot.x > 180f && rot.x < (360f - maxRotationAngle))
+        else if (rot.x > 180f && rot.x < 345f)
         {
-            rot.x = 360f - maxRotationAngle;
+            rot.x = 345f;
             transform.eulerAngles = rot;
         }
     }
